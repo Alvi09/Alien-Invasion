a29d88c27d6147f94b1a49bd8ec9ca3d7f5ba175
Alvi09 <65634473+Alvi09@users.noreply.github.com>
2021-02-26 00:20:00 -0800

    Add files via upload

--- game_stats.py
+++ game_stats.py
@@ -8,7 +8,7 @@ class GameStats():
         self.reset_stats()
         self.game_active = False
 
-        # High score should never be reset
+        # High score should never be reset (because always comparing it to 0 in game_functions.check_high_score()
         self.high_score = 0
 
     def reset_stats(self):
